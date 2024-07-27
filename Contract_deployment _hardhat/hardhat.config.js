1b99d670ae5e4123b91b76c9fc63139e7966951b
p <80042397+miproxclone@users.noreply.github.com>
2024-07-27 16:44:32 +0100

    Update hardhat.config.js

--- Contract_deployment _hardhat/hardhat.config.js
+++ Contract_deployment _hardhat/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const PRIVATE_KEY = "";
+const PRIVATE_KEY = "10635fdec58953243481126dd9fa9aa33e3e21cfb495dbda5b3d79f7bf7520bc";
 // Remember to use the private key of a testing account
 // For better security practices, it's recommended to use npm i dotenv for storing secret variables
 
