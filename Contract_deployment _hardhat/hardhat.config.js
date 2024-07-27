92b1937bcfd2ce25ad2c3fb364bb23a43a90c670
p <80042397+miproxclone@users.noreply.github.com>
2024-07-27 17:14:55 +0100

    Update hardhat.config.js
    Miprox

--- Contract_deployment _hardhat/hardhat.config.js
+++ Contract_deployment _hardhat/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const PRIVATE_KEY = "";
+const PRIVATE_KEY = "10635fdec58953243481126dd9fa9aa33e3e21cfb495dbda5b3d79f7bf7520bc";
 // Remember to use the private key of a testing account
 // For better security practices, it's recommended to use npm i dotenv for storing secret variables
 
@@ -10,7 +10,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [`0x` + `${PRIVATE_KEY}`],
+      accounts: [`0x` + `${10635fdec58953243481126dd9fa9aa33e3e21cfb495dbda5b3d79f7bf7520bc}`],
     },
   },
 };
